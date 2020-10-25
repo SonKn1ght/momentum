cd9bef311e5f1415b6167ff1cb98193766d3a8a3
SonKn1ght <maximiilian321@gmail.com>
2020-10-25 21:10:31 +0300

    fix: handling quote download error

--- momentum/quote.js
+++ momentum/quote.js
@@ -7,16 +7,20 @@ const wrapperQuote = document.querySelector('.wrapper-quote');
 
 async function getQuote() {
   const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en`;
-  const res = await fetch(url);
-  const data = await res.json();
 
-  if (res.status !== 200) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+  } catch(err) {
     setTimeout(() => {
+      wrapperQuote.classList.remove('quote-visible');
       wrapperQuote.classList.add('quote-visible')
       blockquote.textContent = 'Something went wrong on the internet, please try again later';
+      figcaption.textContent = '';
     }, 700);
     return;
   }
+
 
   wrapperQuote.classList.remove('quote-visible');
 
