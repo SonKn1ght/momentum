5b95db0eee73fb6478d2fd68eaf811cd2caaeb18
SonKn1ght <maximiilian321@gmail.com>
2020-10-25 23:01:31 +0300

    feat: quote loading indication

--- momentum/quote.js
+++ momentum/quote.js
@@ -8,27 +8,37 @@ const wrapperQuote = document.querySelector('.wrapper-quote');
 async function getQuote() {
   const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en`;
 
-  try {
-    const res = await fetch(url);
-    var data = await res.json();
-  } catch(err) {
-    setTimeout(() => {
-      wrapperQuote.classList.remove('quote-visible');
-      wrapperQuote.classList.add('quote-visible')
-      blockquote.textContent = 'Something went wrong on the internet, please try again later';
-      figcaption.textContent = '';
-    }, 700);
-    return;
-  }
-
-
+  // api виснет добавил заглушку на время отрабатывания промиса
   wrapperQuote.classList.remove('quote-visible');
-
   setTimeout(() => {
     wrapperQuote.classList.add('quote-visible')
-    blockquote.textContent = data.quoteText;
-    figcaption.textContent = data.quoteAuthor;
-  }, 700);
+    blockquote.textContent = 'Quote loading...';
+    figcaption.textContent = '';
+  }, 650);
+
+    try {
+      const res = await fetch(url);
+      var data = await res.json();
+    } catch(err) {
+      setTimeout(() => {
+        wrapperQuote.classList.remove('quote-visible');
+        wrapperQuote.classList.add('quote-visible')
+        blockquote.textContent = 'Something went wrong on the internet, please try again later';
+        figcaption.textContent = '';
+      }, 700);
+      return;
+    }
+
+
+    wrapperQuote.classList.remove('quote-visible');
+
+    setTimeout(() => {
+      wrapperQuote.classList.add('quote-visible')
+      blockquote.textContent = data.quoteText;
+      figcaption.textContent = data.quoteAuthor;
+    }, 705);
+
+
 }
 
 document.addEventListener('DOMContentLoaded', getQuote);
